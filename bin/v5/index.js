cf220d1f860a69207bad2d8c874177853c79b8e4
keshavsoft <nknnkumar@yahoo.com>
2026-08-08 09:40:47 +0530

    feat: add v5 API configuration and supporting regex extraction rules

---
+++ bin/v5/index.js
@@ -0,0 +1,7 @@
+import extractRegex from './extractRegex.js';
+import fileNamesJson from './fileNames.json' with {type: 'json'};
+import outputStructureJson from './outputStructure.json' with {type: 'json'};
+
+const apiVersion = 'v4';
+
+export { extractRegex, fileNamesJson, outputStructureJson, apiVersion };
