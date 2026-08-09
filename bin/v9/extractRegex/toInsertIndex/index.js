7b453b91969c0ef98230dbda3ba4eb86b8609e55
keshavsoft <nknnkumar@yahoo.com>
2026-08-09 09:56:46 +0530

    parse start

---
+++ bin/v9/extractRegex/toInsertIndex/index.js
@@ -0,0 +1,86 @@
+export default {
+    fromRoutesJsEnd: {
+        import: [
+            "importLines.firstLineIndex",
+            "importLinesFromNpm.lastLineIndex",
+            "firstLineIndex"
+        ],
+        consumption: [
+            "useLines.firstLineIndex",
+            "variablesDeclareHereLines.lastLineIndex",
+            "lastLineIndex"
+        ]
+    },
+    fromAppJs: {
+        import: [
+            "importLines.firstLineIndex",
+            "importLinesFromNpm.lastLineIndex",
+            "firstLineIndex"
+        ],
+        consumption: [
+            "useLines.firstLineIndex",
+            "variablesDeclareHereLines.lastLineIndex",
+            "lastLineIndex"
+        ]
+    },
+    fromRoutesJs: {
+        import: [
+            "importLines.firstLineIndex",
+            "importLinesFromNpm.lastLineIndex",
+            "firstLineIndex"
+        ],
+        consumption: [
+            "useLines.firstLineIndex",
+            "variablesDeclareHereLines.lastLineIndex",
+            "lastLineIndex"
+        ]
+    },
+    fromEndPointsJs: {
+        import: [
+            "importLines.firstLineIndex",
+            "importLinesFromNpm.lastLineIndex",
+            "firstLineIndex"
+        ],
+        consumption: [
+            "useLines.firstLineIndex",
+            "variablesDeclareHereLines.lastLineIndex",
+            "lastLineIndex"
+        ]
+    },
+    tableGetShowAll: {
+        import: [
+            "importLines.firstLineIndex",
+            "importLinesFromNpm.lastLineIndex",
+            "firstLineIndex"
+        ],
+        consumption: [
+            "useLines.firstLineIndex",
+            "variablesDeclareHereLines.lastLineIndex",
+            "lastLineIndex"
+        ]
+    },
+    tableGetFind: {
+        import: [
+            "importLines.firstLineIndex",
+            "importLinesFromNpm.lastLineIndex",
+            "firstLineIndex"
+        ],
+        consumption: [
+            "useLines.firstLineIndex",
+            "variablesDeclareHereLines.lastLineIndex",
+            "lastLineIndex"
+        ]
+    },
+    tablePostShowAll: {
+        import: [
+            "importLines.firstLineIndex",
+            "importLinesFromNpm.lastLineIndex",
+            "firstLineIndex"
+        ],
+        consumption: [
+            "useLines.firstLineIndex",
+            "variablesDeclareHereLines.lastLineIndex",
+            "lastLineIndex"
+        ]
+    }
+};
---
+++ test/v8/extractRegex.js
@@ -0,0 +1,5 @@
+import { 
+    extractRegex, 
+} from '../../index.js';
+
+console.log("extractRegex:", extractRegex().searchRules.fromAppJs);
