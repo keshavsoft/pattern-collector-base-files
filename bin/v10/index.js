78c51d001b824c5805901e62f97626a19b08bbfb
keshavsoft <nknnkumar@yahoo.com>
2026-08-09 12:39:30 +0530

    narrationJson

--- bin/v10/index.js
+++ bin/v10/index.js
@@ -1,7 +1,11 @@
 import extractRegex from './extractRegex/extractRegex.js';
 import fileNamesJson from './fileNames.json' with {type: 'json'};
 import outputStructureJson from './outputStructure.json' with {type: 'json'};
+import narrationJson from './narration.json' with {type: 'json'};
 
-const apiVersion = 'v7';
+const apiVersion = 'v10';
 
-export { extractRegex, fileNamesJson, outputStructureJson, apiVersion };
+export {
+    extractRegex, fileNamesJson, outputStructureJson,
+    apiVersion, narrationJson
+};
